d4d0ab27cecd434a911e64bfcae91da75aedcba0
fabianogadenz <fabianomgadenz@gmail.com>
2019-10-04 11:08:38 -0300

    aula 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgifNgforComponent } from './ngif-ngfor/ngif-ngfor.component';
+
+const appRoutes: Routes = [
+  { path: 'data-binding', component: DataBindingComponent },
+  { path: 'segundo-componente', component: SegundoComponenteComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent,
+    SegundoComponenteComponent,
+    NgifNgforComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/ngif-ngfor/ngif-ngfor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngif-ngfor',
+  templateUrl: './ngif-ngfor.component.html',
+  styleUrls: ['./ngif-ngfor.component.css']
+})
+export class NgifNgforComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
